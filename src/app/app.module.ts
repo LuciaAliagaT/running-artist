d7f5ceb2f8a8bed1072a47d0d31cd0dcd0e2f968
Lucía A <91093842+LuciaAliagaT@users.noreply.github.com>
2023-09-30 23:21:26 -0500

    feat: added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListOffersComponent } from './components/list-offers/list-offers.component';
+import { MaterialModule } from 'src/shared/material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ListOffersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/http-data.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { catchError, Observable, retry, throwError } from 'rxjs';
+import { Offers } from '../models/offers.model';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpDataService {
+  baseUrl: string = environment.baseURL;
+  constructor(private http: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  handlerError(error: HttpErrorResponse){
+    if(error.error instanceof ErrorEvent){
+      console.log(`An errorr ocurred ${error.status}, body was ${error.error}`);
+    } else{
+      console.log(`Backend returned code ${error.status}, body was ${error.error}`);
+    }
+    return throwError('Something happened with request, please try again later');
+
+  }
+
+  //Offer Crud
+  getList(): Observable<Offers[]>{
+    return this.http.get<Offers[]>(`${this.baseUrl}/offers`)
+    .pipe(retry(2), catchError(this.handlerError))
+  }
+  //OFFER  -> GET
+  getOffer(id: string): Observable<Offers>{
+    return this.http.get<Offers>(`${this.baseUrl}/${id}`)
+    .pipe(retry(2), catchError(this.handlerError))
+  }
+  //CREATE → POST
+  createOffer(item: any): Observable<Offers>{
+    return this.http.post<Offers>(this.baseUrl, JSON.stringify(item), this.httpOptions)
+    .pipe(retry(2), catchError(this.handlerError));
+  }
+  //DELETE → DELETE
+  deleteOffer(id: string): Observable<Offers>{
+    return this.http.delete<Offers>(`${this.baseUrl}/${id}`, this.httpOptions)
+    .pipe(retry(2), catchError(this.handlerError))
+  }
+  //UPDATE → DELETE
+
+  updateMovie(id: string, item: any): Observable<Offers>{
+    return this.http.put<Offers>(this.baseUrl + '/' + id, JSON.stringify(item), this.httpOptions)
+    .pipe(retry(2), catchError(this.handlerError));
+  }
+
+
+}
